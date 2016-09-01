671084a83e307313ef4c24f0f0ec9e2bc206571e
Benjamin Esquivel <benjamin.esquivel@linux.intel.com>
2016-09-01 14:48:36 -0500

    setup.py additions in prep for PyPI
    in case this module goes to PyPI, here's some useful information to
    begin with.
    
    Signed-off-by: Benjamin Esquivel <benjamin.esquivel@linux.intel.com>

--- setup.py
+++ setup.py
@@ -7,6 +7,14 @@ setup (name = 'cleware',
        version = '2.3',
        author      = "Folkert van Heusden",
        description = """Cleware library bindings for Python""",
+       license='AGPLv3',
        ext_modules = [cleware_module],
        py_modules = ["cleware"],
+       classifiers = [
+          'License :: OSI Approved :: GNU Affero General Public License v3',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+       ]
        )
